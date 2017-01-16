48fd1431297c9a1e7c3806c38556e497647ff677
mirror12k <mirror12k@gmail.com>
2017-01-15 16:18:30 -0800

    - implemented cursor movement left/right and up/down  - implemented editting data  - implemented writing data to file

--- bedit.py
+++ bedit.py
@@ -9,43 +9,80 @@ def read_file(filepath):
 		text = bytes(f.read())
 	return text
 
+def write_file(filepath, text):
+	with open(filepath, 'wb') as f:
+		f.write(text)
+
+
+
+
 def rep_data(data):
 	return "%02X" % data
 
+def display_byte(scr, data, index, width=8):
+	index_y = index / width
+	index_x = index % width
+	scr.addstr(index_y, index_x * 3, rep_data(data[index]) + ' ')
+
 def display_bytes(scr, data, width=8):
 	line = 0
-	print 'debug:', data
-	while line < len(data) / width and line < 40:
-		for i in range(0, width):
-			if line * width + i < len(data):
-				print data[line * width + i]
-				scr.addstr(line, i * 3, rep_data(data[line * width + i]) + ' ')
-		line += 1
+	for i in range(0, len(data)):
+		display_byte(scr, data, i, width)
 
 
 
+def place_cursor(scr, cursor_index, width=8):
+	cursor_y = cursor_index / 2 / width
+	cursor_x = cursor_index / 2 % width
+	cursor_offset = cursor_index % 2
+	scr.addstr(cursor_y, cursor_x * 3 + cursor_offset, '')
+
+def edit_byte(data, cursor_index, key):
+	if cursor_index % 2 == 0:
+		data[cursor_index / 2] = (int(key, 16) << 4) | (data[cursor_index / 2] & 0xf)
+	else:
+		data[cursor_index / 2] = int(key, 16) | (data[cursor_index / 2] & 0xf0)
 
 def bedit_main(scr):
+	cursor_index = 0
+	width = 16
 	scr.clear()
 
 	if len(sys.argv) > 1:
-		data = read_file(sys.argv[1])
-		scr.addstr(5, 0, 'read file: '+sys.argv[1])
+		filepath = sys.argv[1]
+		data = read_file(filepath)
+		scr.addstr(50, 1, 'read file: '+filepath)
 	
 	data = [ ord(c) for c in data ]
 
 	#scr.addstr(0, 0, str(data))
 
-	display_bytes(scr, data, 16)
+	display_bytes(scr, data, width)
+	place_cursor(scr, cursor_index, width)
 	
 	scr.refresh()
 	k = scr.getkey()
 	while k != 'q':
-		#scr.addstr(0, 0, k)
-		
+		if k == 'KEY_LEFT':
+			cursor_index -= 1
+		elif k == 'KEY_RIGHT':
+			cursor_index += 1
+		elif k == 'KEY_UP':
+			cursor_index -= width * 2
+		elif k == 'KEY_DOWN':
+			cursor_index += width * 2
 
-		scr.addstr(0, 0, '')
+		elif len(k) == 1 and ((k >= '0' and k <= '9') or (k >= 'a' and k <= 'f')):
+			edit_byte(data, cursor_index, k)
+			display_byte(scr, data, cursor_index / 2, width)
+			cursor_index += 1
 
+		elif k == 'w':
+			write_file(filepath, ''.join([ chr(c) for c in data ]))
+			scr.addstr(50, 1, 'wrote file: '+filepath)
+
+		place_cursor(scr, cursor_index, width)
+		#scr.addstr(0, 0, '')
 
 		scr.refresh()
 		k = scr.getkey()
